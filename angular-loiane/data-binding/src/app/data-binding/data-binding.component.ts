dc7c0843fa1691c1f833db88ead56de6a6fb41ed
Bruno Paulo Costa Lima <bplima@unicid.rede1.br>
2017-09-06 18:08:19 -0300

    Chapter Input/Output property

--- angular-loiane/data-binding/src/app/data-binding/data-binding.component.ts
+++ angular-loiane/data-binding/src/app/data-binding/data-binding.component.ts
@@ -15,13 +15,15 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class DataBindingComponent implements OnInit {
+
   url: string = 'http://loiane.com';
   cursoAngular: boolean = true;
   urlImagem = 'https://static.omelete.uol.com.br/media/filer_public_thumbnails/filer_public/e3/b0/e3b0a3dc-4603-4e63-bcbc-cd42c334f3c7/09_09_destaque.png__250x250_q85_crop_subsampling-2_upscale.jpg';
   valorAtual: string = '';
   valorSalvo: string = '';
   isMouseOver: boolean = false;
- 
+  nomeDoCurso: string = 'Angular';
+
   constructor() { }
 
   getValor() {
