bfc2b3bb1022b5ce7005cd14dff8ef311ff9926e
Bruno Paulo Costa Lima <bplima@unicid.rede1.br>
2017-09-12 12:51:12 -0300

    directives

--- angular-loiane/diretivas/src/app/app.module.ts
+++ angular-loiane/diretivas/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
@@ -21,7 +22,8 @@ import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.co
   ],
   imports: [
     BrowserModule,
-    AlertModule.forRoot()
+    AlertModule.forRoot(), 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
