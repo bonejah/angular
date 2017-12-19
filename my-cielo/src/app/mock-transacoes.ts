b97e9e807d4c39ca2c9852aabb0534014f79b72a
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2017-12-19 17:22:35 -0200

    development...

--- my-cielo/src/app/mock-transacoes.ts
+++ my-cielo/src/app/mock-transacoes.ts
@@ -1,10 +1,110 @@
 import { Transacao } from './transacao';
 
-export const transacoes: Transacao[] = [
-  { id_transaction: 1, id_payment: 4, name_consumer: 'Bruno Lima', amount: 7000 },
-  { id_transaction: 2, id_payment: 3, name_consumer: 'Leticia Lima', amount: 8000 },
-  { id_transaction: 3, id_payment: 2, name_consumer: 'Bianca Lima', amount: 9000 },
-  { id_transaction: 4, id_payment: 1, name_consumer: 'Matheus Lima', amount: 10000 }
+export const transacoes = [
+  {
+    'MerchantOrderId': '2014111703',
+    'Customer': {
+      'Name': '[Guest]'
+    },
+    'Payment': {
+      'ServiceTaxAmount': 0,
+      'Installments': 1,
+      'Interest': 0,
+      'Capture': false,
+      'Authenticate': false,
+      'Recurrent': false,
+      'CreditCard': {
+        'CardNumber': '455187******0181',
+        'Holder': 'Teste Holder',
+        'ExpirationDate': '12/2021',
+        'SaveCard': false,
+        'Brand': 'Visa'
+      },
+      'Tid': '0407120858677',
+      'ProofOfSale': '858677',
+      'AuthorizationCode': '408180',
+      'SoftDescriptor': '123456789ABCD',
+      'Provider': 'Simulado',
+      'PaymentId': 'eb616a9f-00bc-437d-bbf8-0f5e8a3e716c',
+      'Type': 'CreditCard',
+      'Amount': 15700,
+      'ReceivedDate': '2017-04-07 12:08:58',
+      'Currency': 'BRL',
+      'Country': 'BRA',
+      'ReturnCode': '4',
+      'ReturnMessage': 'Operation Successful',
+      'Status': 1,
+      'Links': [
+        {
+          'Method': 'GET',
+          'Rel': 'self',
+          'Href': 'https://apiquerydev.cieloecommerce.cielo.com.br/1/sales/eb616a9f-00bc-437d-bbf8-0f5e8a3e716c'
+        },
+        {
+          'Method': 'PUT',
+          'Rel': 'capture',
+          'Href': 'https://apidev.cieloecommerce.cielo.com.br/1/sales/eb616a9f-00bc-437d-bbf8-0f5e8a3e716c/capture'
+        },
+        {
+          'Method': 'PUT',
+          'Rel': 'void',
+          'Href': 'https://apidev.cieloecommerce.cielo.com.br/1/sales/eb616a9f-00bc-437d-bbf8-0f5e8a3e716c/void'
+        }
+      ]
+    }
+  },
+  {
+    'MerchantOrderId': '2017191212',
+    'Customer': {
+      'Name': '[Guest]'
+    },
+    'Payment': {
+      'ServiceTaxAmount': 0,
+      'Installments': 1,
+      'Interest': 0,
+      'Capture': false,
+      'Authenticate': false,
+      'Recurrent': false,
+      'CreditCard': {
+        'CardNumber': '455187******0181',
+        'Holder': 'Teste Holder',
+        'ExpirationDate': '12/2021',
+        'SaveCard': false,
+        'Brand': 'Visa'
+      },
+      'Tid': '0407120858677',
+      'ProofOfSale': '858677',
+      'AuthorizationCode': '408180',
+      'SoftDescriptor': '123456789ABCD',
+      'Provider': 'Simulado',
+      'PaymentId': 'eb616a9f-00bc-437d-bbf8-0f5e8a3e716c',
+      'Type': 'CreditCard',
+      'Amount': 15700,
+      'ReceivedDate': '2017-04-07 12:08:58',
+      'Currency': 'BRL',
+      'Country': 'BRA',
+      'ReturnCode': '4',
+      'ReturnMessage': 'Operation Successful',
+      'Status': 1,
+      'Links': [
+        {
+          'Method': 'GET',
+          'Rel': 'self',
+          'Href': 'https://apiquerydev.cieloecommerce.cielo.com.br/1/sales/eb616a9f-00bc-437d-bbf8-0f5e8a3e716c'
+        },
+        {
+          'Method': 'PUT',
+          'Rel': 'capture',
+          'Href': 'https://apidev.cieloecommerce.cielo.com.br/1/sales/eb616a9f-00bc-437d-bbf8-0f5e8a3e716c/capture'
+        },
+        {
+          'Method': 'PUT',
+          'Rel': 'void',
+          'Href': 'https://apidev.cieloecommerce.cielo.com.br/1/sales/eb616a9f-00bc-437d-bbf8-0f5e8a3e716c/void'
+        }
+      ]
+    }
+  }
 ];
 
 
