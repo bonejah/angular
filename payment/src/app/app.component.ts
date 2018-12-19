4a7134d15950400b4c6070350ab01b483b2d6c79
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2018-12-19 17:21:39 -0200

    add Validators

--- payment/src/app/app.component.ts
+++ payment/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { reject } from 'q';
 
 @Component({
   selector: 'payment-form',
@@ -10,6 +11,8 @@ export class AppComponent implements OnInit {
   paymentForm: FormGroup;
   displayMessage: string;
   isPageReady: boolean = false;
+  // namePattern: "^[a-zA-Z]+$";
+  namePattern: "/^[a-zA-Z]+$/";
 
   constructor(
     private formBuilder: FormBuilder
@@ -23,19 +26,29 @@ export class AppComponent implements OnInit {
   initForm() {
     return new Promise((resolve, reject) => {
       this.paymentForm = this.formBuilder.group({
-          NameCard: [null, Validators.compose([Validators.required])],
-          CardNumber: [null, Validators.compose([Validators.required])],
+          NameCard: [null, Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z \-\']+')])],
+          CardNumber: [null, Validators.compose([Validators.required, Validators.minLength(16), Validators.maxLength(16)])],
           Month: null,
           Year: null,
           Cvv: null
       });
-      console.log(this.paymentForm)
+
+      console.log('paymentForm', this.paymentForm)
       resolve();
+    })
+    .catch(() =>{
+      reject('Error......!!!!!');
     });
   }
 
   submitForm() {
-    console.log('clicou aqui')
+    if (this.paymentForm.invalid) {
+      this.displayMessage = "Payment Failed!";  
+      return false;
+    }
+    
     this.displayMessage = "Payment Successful!";
+    return this.paymentForm.valid;
   }
+
 }
